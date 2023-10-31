501709a726462f34790a1188bd9c7a23c6287bf2
Davidson Campos <davidsonc@gmail.com>
2023-10-31 17:27:35 -0300

    feat: level 13

--- src/server/controllers/cidades/index.ts
+++ src/server/controllers/cidades/index.ts
@@ -1,7 +1,13 @@
+import * as DeleteById from './DeleteById';
+import * as updateById from './UpdateById';
+import * as getById from './GetById';
 import * as create from './Create';
 import * as getAll from './GetAll';
 
 export const CidadesController = {
+    ...DeleteById,
+    ...updateById,
+    ...getById,
     ...create,
     ...getAll,
 };
