d6676b34c5b914383e7199d64b3446b1f52f3b10
Davidson Campos <davidsonc@gmail.com>
2023-11-09 20:17:01 -0300

    feat: providers finished

--- src/server/controllers/cidades/DeleteById.ts
+++ src/server/controllers/cidades/DeleteById.ts
@@ -1,24 +1,37 @@
 import { StatusCodes } from 'http-status-codes';
-import { Request,  Response } from 'express';
+import { Request, Response } from 'express';
 import * as yup from 'yup';
 import { validation } from '../../shared/middleware';
-
+import { CidadesProvider } from '../../database/providers/cidades';
 
 interface IParamProps {
     id?: number;
 }
 export const deleteByIdValidation = validation((getSchema) => ({
-    params: getSchema<IParamProps>(yup.object().shape({
-        id: yup.number().integer().required().moreThan(0),
-    })),
+    params: getSchema<IParamProps>(
+        yup.object().shape({
+            id: yup.number().integer().required().moreThan(0),
+        })
+    ),
 }));
 
 export const deleteById = async (req: Request<IParamProps>, res: Response) => {
-    if(Number(req.params.id) === 9999) return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-        errors: {
-            default: 'Registro não encontrado'
-        }
-    });
+    if (Number(req.params.id) === 9999)
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            errors: {
+                default: 'Registro não encontrado',
+            },
+        });
+
+    const result = await CidadesProvider.deleteById(Number(req.params.id));
+
+    if (result instanceof Error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            errors: {
+                default: result.message,
+            },
+        });
+    }
 
     return res.status(StatusCodes.NO_CONTENT).send();
 };
--- src/server/controllers/cidades/GetAll.ts
+++ src/server/controllers/cidades/GetAll.ts
@@ -1,8 +1,8 @@
 import { StatusCodes } from 'http-status-codes';
-import { Request,  Response } from 'express';
+import { Request, Response } from 'express';
 import * as yup from 'yup';
 import { validation } from '../../shared/middleware';
-
+import { CidadesProvider } from '../../database/providers/cidades';
 
 interface IQueryProps {
     page?: number;
@@ -10,21 +10,33 @@ interface IQueryProps {
     filter?: string;
 }
 export const getAllValidation = validation((getSchema) => ({
-    query: getSchema<IQueryProps>(yup.object().shape({
-        page: yup.number().notRequired().moreThan(0),
-        limit: yup.number().notRequired().moreThan(0),
-        filter: yup.string().notRequired(),
-    })),
+    query: getSchema<IQueryProps>(
+        yup.object().shape({
+            page: yup.number().optional().moreThan(0),
+            limit: yup.number().optional().moreThan(0),
+            filter: yup.string().optional(),
+        })
+    ),
 }));
 
-export const getAll = async (req: Request<{}, {}, {}, IQueryProps>, res: Response) => {
+export const getAll = async (
+    req: Request<{}, {}, {}, IQueryProps>,
+    res: Response
+) => {
     res.setHeader('access-control-expose-headers', 'x-total-count');
     res.setHeader('x-total-count', 1);
 
-    return res.status(StatusCodes.OK).json([
-        {
-            id: 1,
-            nome: 'Caxias do Sul',
-        }
-    ]);
+    const result = await CidadesProvider.getAll();
+
+    console.log(result);
+
+    if (result instanceof Error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            errors: {
+                default: result.message,
+            },
+        });
+    }
+
+    return res.status(StatusCodes.OK).json(result);
 };
