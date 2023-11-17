6bdc94facfe6049f2453460f0f180011527c0c25
Davidson Campos <davidsonc@gmail.com>
2023-11-17 15:47:18 -0300

    feat: xp file explorer

---
+++ xp/2/config/.htusers.php
@@ -0,0 +1,7 @@
+<?php 
+	// ensure this file is being included by a parent file
+	if( !defined( '_JEXEC' ) && !defined( '_VALID_MOS' ) ) die( 'Restricted access' );
+	$GLOBALS["users"]=array(
+	array('admin','$2a$08$aakc2izkRTXnh4kYSELLGeDU6F7WLuTOB.Qy0OBERio1nnrqA179C','C:/xampp/htdocs','http://localhost','1','','7',1),
+); 
+?>
