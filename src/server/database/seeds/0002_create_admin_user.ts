54dfa6ff69ecda0f9776fc235b280b0ed31c57fb
Davidson Campos <davidsonc@gmail.com>
2023-11-21 12:06:33 -0300

    feat: seed user admin refactoring with hash pword

--- src/server/database/seeds/0002_create_admin_user.ts
+++ src/server/database/seeds/0002_create_admin_user.ts
@@ -1,5 +1,5 @@
-import { PasswordCrypto } from './../../shared/services/PasswordCrypto';
 import { Knex } from 'knex';
+import { PasswordCrypto } from './../../shared/services/PasswordCrypto';
 
 import { ETableNames } from '../ETableNames';
 
@@ -11,16 +11,20 @@ export const seed = async (knex: Knex) => {
 
     const usuariosList = ['admin, 123456, admin@gmail.com'];
 
-    const usersToInsert = usuariosList.map((usuario) => {
+    const usersToInsert = await usuariosList.map((usuario) => {
         const match = usuario.match(/^(.*),\s*(.*),\s*(.*)$/);
 
         if (match) {
             const [_, nome, senha, email] = match;
-            const hashadPassword = PasswordCrypto.hashPassword(senha);
-            return { nome, hashadPassword, email };
+            return { nome, senha, email };
         }
         throw new Error();
     });
+
+    for (let index = 0; index < usersToInsert.length; index++) {
+        const element = usersToInsert[index];
+        element.senha = await PasswordCrypto.hashPassword(element.senha);
+    }
 
     await knex(ETableNames.usuario).insert(usersToInsert);
 };
