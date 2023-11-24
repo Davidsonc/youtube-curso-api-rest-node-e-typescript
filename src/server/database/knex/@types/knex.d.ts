77c49840f69e0160d69586d45ce7570d06f55810
Davidson Campos <davidsonc@gmail.com>
2023-11-24 13:07:09 -0300

    feat: cors config

--- src/server/database/knex/@types/knex.d.ts
+++ src/server/database/knex/@types/knex.d.ts
@@ -1,10 +1,11 @@
-import { ICidade } from '../../models';
-import { IPessoa } from '../../models';
+import { ICidade } from '../../models/Cidade';
+import { IPessoa } from '../../models/Pessoa';
+import { IUsuario } from '../../models/Usuario';
 
 declare module 'knex/types/table' {
     interface Tables {
         cidade: ICidade;
         pessoa: IPessoa;
-        //usuario: IUsuario
+        usuario: IUsuario
     }
 }
